88d4aff8bbd37d27c4f5f5236508df4dacf2ee70
ms.kuznetcov <ms.kuznetcov@stalcom.com>
2024-04-10 15:31:33 +0300

    Initial commit

---
+++ src/main/java/com/example/taskira/TaskiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskira/TaskiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
